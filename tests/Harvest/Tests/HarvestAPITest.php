aaf5745afaca7c237259fd5441862f92d6a8a032
Peter Siska <pesche@gridonic.ch>
2013-11-05 10:26:27 +0100

    Added test stub class & method

---
+++ tests/Harvest/Tests/HarvestAPITest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Harvest\Tests;
+
+/**
+ * HarvestAPI test cases
+ */
+class HarvestAPITest extends \PHPUnit_Framework_TestCase
+{
+    public function testNothing() {
+        $this->assertTrue(true);
+    }
+}
